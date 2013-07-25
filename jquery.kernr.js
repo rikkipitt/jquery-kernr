122acdb9048de8749bd6486ebc1d23808bbfc87c
Rikki Pitt <rikki@studiomohu.com>
2013-07-26 00:06:00 +0100

    Updated RegEx to allow elements to contain HTML

--- jquery.kernr.js
+++ jquery.kernr.js
@@ -7,12 +7,12 @@
         if (kerning) {
           $.each(kerning, function(letters, distance) {
             var split = letters.split(""),
-              regex = new RegExp('([' + split[0] + '])([' + split[1] + '])');
-            result = $(that).text().replace(regex, "<span style=\"margin-right: " + distance + ";\">$1</span>$2");
-            $(that).text(result);
+              regex = new RegExp('('+split[0]+'(?![^<]*\\>))('+split[1]+'(?![^<]*\\>))');
+            result = $(that).html().replace(regex, "<span style=\"margin-right: " + distance + ";\">$1</span>$2");
+            $(that).html(result);
           });
         } else {
-          result = $(that).text();
+          result = $(that).html();
         }
         return result;
       });
