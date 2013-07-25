8cd58205e85fd7fd627bd29f1eb332f31309eaac
Rikki Pitt <rikki@studiomohu.com>
2013-07-25 22:57:19 +0100

    Added object check and fallback

--- jquery.kernr.js
+++ jquery.kernr.js
@@ -4,12 +4,16 @@
       $(this).html(function() {
         var that    = this,
           kerning = $(this).data('kernr');
-        $.each(kerning, function(letters, distance) {
-          var split = letters.split(""),
-            regex = new RegExp('([' + split[0] + '])([' + split[1] + '])');
-          result = $(that).text().replace(regex, "<span style=\"margin-right: " + distance + ";\">$1</span>$2");
-          $(that).text(result);
-        });
+        if (kerning) {
+          $.each(kerning, function(letters, distance) {
+            var split = letters.split(""),
+              regex = new RegExp('([' + split[0] + '])([' + split[1] + '])');
+            result = $(that).text().replace(regex, "<span style=\"margin-right: " + distance + ";\">$1</span>$2");
+            $(that).text(result);
+          });
+        } else {
+          result = $(that).text();
+        }
         return result;
       });
     });
